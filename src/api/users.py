423691d1cf616a7e183ff0afac79df9d6afef3d1
Hajarelmouddene <hajarelmouddene@gmail.com>
2021-06-22 14:08:26 -0700

    added users put & delete routes. Updated unit tests for users.

--- src/api/users.py
+++ src/api/users.py
@@ -26,7 +26,7 @@ user = api.model(
 
 
 class UsersList(Resource):
-    @api.expect(user, validate=True)  # new
+    @api.expect(user, validate=True)  
     def post(self):
         post_data = request.get_json()
         username = post_data.get("username")
@@ -63,6 +63,36 @@ class Users(Resource):
         if not user:
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
+    
+    def delete(self, user_id):
+        response_object = {}
+        user = User.query.filter_by(id=user_id).first()
 
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        
+        db.session.delete(user)
+        db.session.commit()
+
+        response_object['message'] = f"{user.email} was removed."
+        return response_object, 200 
+
+    @api.expect(user, validate=True)
+    def put(self, user_id):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        
+        user.username = username
+        user.email = email
+        db.session.commit()
+
+        response_object["message"] = f"{user.id} was updated!"
+        return response_object, 200
 
 api.add_resource(Users, "/users/<int:user_id>")
